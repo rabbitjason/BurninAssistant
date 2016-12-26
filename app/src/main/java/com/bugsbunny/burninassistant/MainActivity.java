bf16fee9aa2077fb469b86d31f130e2452ba3a63
rabbitjason <lipple-server@Lipple-ServerdeMac-mini.local>
2016-12-26 17:09:57 +0800

    add function

--- app/src/main/java/com/bugsbunny/burninassistant/MainActivity.java
+++ app/src/main/java/com/bugsbunny/burninassistant/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -20,7 +21,7 @@ import com.bugsbunny.burninassistant.view.IPlanView;
 
 import java.text.SimpleDateFormat;
 
-public class MainActivity extends BaseActivity implements IPlanView, View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements IPlanView, View.OnClickListener {
     private View llDuration, llInterval, llMusicMore;
     private TextView tvDurationHour, tvDurationMinute, tvIntervalMinute;
     private TextView tvCountdownTime, tvTotalTime, tvLastTime, tvMusicName, tvMusicDetail;
